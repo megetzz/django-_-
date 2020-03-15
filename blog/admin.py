feeefd9605b891d15eaea25697d50fe145ad2e73
megetzz <ma.1971250916@qq.com>
2020-03-15 12:12:24 +0800

    django

---
+++ blog/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# Register your models here.
+
+
+from blog.models import  Article
+
+admin.site.register(Article)
---
+++ blog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+from . import models
+
+def index(request):
+    article=models.Article.objects.get(pk=1)
+    # return HttpResponse('hello world !!!')
+    return render(request, 'blog/index.html', {'article':article})
+
+
